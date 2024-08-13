609b9e1208d06fe829d6bc043e3c3f7495bb295a
Omar Faruk <omarfaruk65142@gmail.com>
2024-08-14 02:03:09 +0600

    Create react responsive banner

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
+import Header from "../Pages/Shared/Header";
 
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>

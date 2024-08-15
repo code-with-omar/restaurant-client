ff9e11f715381d2d4de812e4a6431c0abb72c986
Omar Faruk <omarfaruk65142@gmail.com>
2024-08-16 01:57:59 +0600

    Create cover for multipule page as a shared components

--- src/Pages/Menus/Menus/Menus.jsx
+++ src/Pages/Menus/Menus/Menus.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import img from "./../../../assets/menu/banner3.jpg"
 const Menus = () => {
     return (
         <div>
@@ -6,7 +8,7 @@ const Menus = () => {
                 <title>Bistro | menus</title>
                 <link rel="canonical" href="https://www.tacobell.com/" />
             </Helmet>
-           
+           <Cover img={img} title="OUR MENU" describe="Would you like to try a dish?"></Cover>
         </div>
     );
 };
---
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,20 @@
+const Cover = ({ img, title, describe }) => {
+    return (
+        <div
+            className="hero h-[450px] lg:h-[600px] md:h-[550px] bg-no-repeat bg-cover"
+            style={{
+                backgroundImage: `url(${img})`,
+            }}>
+            <div className="hero-overlay bg-opacity-0"></div>
+            <div className="hero-content  text-center bg-slate-900 bg-opacity-50  w-3/4 mx-auto px-12 lg:px-48 py-12 lg:py-24 self-center rounded">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-white text-4xl md:text-6xl lg:text-7xl font-bold">{title}</h1>
+                    <p className="mb-5 text-base md:text-lg lg:text-xl text-white font-semibold">{describe}</p>
+      
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cover;

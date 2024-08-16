f3a078ea23e2cfeb77bc3b5a644c16ce0b4cfe6d
Omar Faruk <omarfaruk65142@gmail.com>
2024-08-16 12:52:35 +0600

    Menu page is create without button

--- src/Pages/Menus/Menus/Menus.jsx
+++ src/Pages/Menus/Menus/Menus.jsx
@@ -3,8 +3,7 @@ import Cover from '../../Shared/Cover/Cover';
 import img from "./../../../assets/menu/banner3.jpg"
 import useHooks from '../../../Hooks/useHooks';
 import SectionTitle from '../../../components/SectionTitle';
-import MenuItem from '../../Shared/MenuItem/MenuItem';
-import MenuCover from '../../Shared/MenuCover/MenuCover';
+import MenuCategory from '../../Shared/MenuCategory/MenuCategory';
 const Menus = () => {
     const [menu] = useHooks()
     const salad = menu.filter(item => item.category === "salad")
@@ -20,12 +19,11 @@ const Menus = () => {
             </Helmet>
             <Cover img={img} title="OUR MENU" describe="Would you like to try a dish?"></Cover>
             <SectionTitle subHeading={"Don't miss"} heading={"TODAY'S OFFER"}></SectionTitle>
-            <div className="grid md:grid-cols-2 gap-10 w-11/12 xl:w-4/5 lg:w-4/5 mx-auto">
-                {
-                    offered.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-                }
-            </div>
-            <MenuCover title="DESSERTS" details="Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></MenuCover>
+            <MenuCategory items={offered}></MenuCategory>
+            <MenuCategory items={desserts} title="DESSERTS" details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></MenuCategory>
+            <MenuCategory items={pizza} title="PIZZA" details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></MenuCategory>
+            <MenuCategory items={salad} title="SALADS" details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></MenuCategory>
+            <MenuCategory items={soup} title="SOUPS" details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></MenuCategory>
         </div>
     );
 };
---
+++ src/Pages/Shared/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,20 @@
+import MenuCover from "../MenuCover/MenuCover";
+import MenuItem from "../MenuItem/MenuItem";
+
+
+const MenuCategory = ({ items, title, details }) => {
+    return (
+        <div>
+             {title && <MenuCover title={title} details={details}></MenuCover>}
+             <div className="grid md:grid-cols-2 gap-10 w-11/12 xl:w-4/5 lg:w-4/5 mx-auto mt-16 md:mt-20 lg:mt-24">
+           
+            {
+                items.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+            }
+        </div>
+        </div>
+        
+    );
+};
+
+export default MenuCategory;

0a66710026f5cf9248c00d22622bd8efdd96e54c
Omar Faruk <omarfaruk65142@gmail.com>
2024-08-19 02:59:29 +0600

    Create Login and logOut authProvider

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,46 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+const auth = getAuth(app);
+export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, passWord) => {
+        setUser(true)
+        return createUserWithEmailAndPassword(auth, email, passWord)
+    }
+    const signIn = (email, password) => {
+        setUser(true);
+        return signInWithEmailAndPassword(email, password)
+    }
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false)
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, } from "react-router-dom";
 import { router } from './Routers/Router';
-import {  HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css'
+import AuthProvider from './Providers/authProvider';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-2xl mx-auto bg-[#161d28]">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-2xl mx-auto bg-[#161d28]">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>,
 )

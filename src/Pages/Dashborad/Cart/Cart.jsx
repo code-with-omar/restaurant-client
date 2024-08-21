f9bffec39feed066fe0f980a33f497f16c69d931
Omar Faruk <omarfaruk65142@gmail.com>
2024-08-22 01:25:26 +0600

    Cart delete item

--- src/Pages/Dashborad/Cart/Cart.jsx
+++ src/Pages/Dashborad/Cart/Cart.jsx
@@ -1,10 +1,41 @@
+import Swal from "sweetalert2";
 import useCart from "../../../Hooks/useCart";
 import { MdDeleteForever } from "react-icons/md";
+import useAxios from "../../../Hooks/useAxios";
 const Cart = () => {
     const [cart] = useCart();
     const totalPrice = cart.reduce((acc, item) => acc + item.price, 0);
-
+    const axiosSecure = useAxios()
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+             
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+                    .catch(error=>{
+                        console.log(error)
+                    })
+            }
+        });
+    }
     return (
+
         <div>
             <div className="flex justify-between">
                 <h2 className="text-[#151515] text-2xl md:text-3xl lg:text-4xl font-bold uppercase">Total Orders: {cart.length}</h2>
@@ -41,9 +72,9 @@ const Cart = () => {
                                 </td>
                                 <td>{item.name}</td>
                                 <td>${item.price}</td>
-                                <th>
-                                    <button className="btn btn-ghost btn-xs text-lg text-white bg-[#B91C1C] rounded-md w-12 h-12 hover:bg-white hover:text-[#B91C1C] transition-colors "><MdDeleteForever className="text-3xl "></MdDeleteForever></button>
-                                </th>
+                                <td>
+                                    <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-xs text-lg text-white bg-[#B91C1C] rounded-md w-12 h-12 hover:bg-white hover:text-[#B91C1C] transition-colors "><MdDeleteForever className="text-3xl "></MdDeleteForever></button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
